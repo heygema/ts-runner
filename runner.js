dc13e2cde707485bb5a0f151550844e84af2e881
Gema <gem.anggada@gmail.com>
2019-12-10 17:57:38 +0700

    checks if builds exist

--- runner.js
+++ runner.js
@@ -1,11 +1,24 @@
 import path from 'path';
+import fs from 'fs';
 
-let whichday = process.argv[2];
-let required = path.join(__dirname, 'lib/', whichday);
-let main = require(required);
+function main() {
+  let whichToRun = process.argv[2];
+  let required = path.join(__dirname, 'lib/', whichToRun);
+  let main = require(required);
 
-let result = main.default && main.default();
+  let result = main.default && main.default();
 
-Promise.resolve(result).then(respond => {
-  console.log(respond);
+  Promise.resolve(result).then(respond => {
+    console.log(respond);
+  });
+}
+
+fs.exists(path.join(__dirname, 'lib/'), exist => {
+  if (exist) {
+    main();
+    return;
+  } else {
+    console.log("build folder doesn't exist");
+    return;
+  }
 });
