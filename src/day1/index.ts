3af6bec44e00d41bca7cdeeec29967254b7202c7
Gema <gem.anggada@gmail.com>
2019-12-10 17:23:11 +0700

    Added workflow to sync files and added types for node

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,3 +1,18 @@
-export default function main() {
+import * as fs from 'fs';
+import * as path from 'path';
+
+function getInput() {
+  let inputPath = path.join(__dirname, './input.txt');
+  fs.readFile(inputPath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log('error >>>', err);
+    }
+
+    console.log('data >>>', data);
+  });
+}
+
+export default async function main() {
+  getInput();
   console.log('hello');
 }
