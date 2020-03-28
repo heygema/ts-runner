436062963b689296d506aa0b3e7359fd0f0f086b
Gema <gem.anggada@gmail.com>
2020-03-28 18:50:24 +0700

    added example transpose matrix

---
+++ src/transpose.ts
@@ -0,0 +1,33 @@
+function transpose(input: number[][]) {
+  let result = [];
+  let step = 0;
+  let prevRow = Array.isArray(input[0]) && input[0].length;
+  let prevColumn = input.length;
+
+  for (let i = 0; i < prevRow; i++) {
+    for (let j = 0; j < prevColumn; j++) {
+      let currentItem = result[step] || [];
+      result[step] = [...currentItem, input[j][i]];
+    }
+    step++;
+  }
+
+  return result;
+}
+
+export default (function() {
+  let result1 = transpose([
+    [1, 2, 3],
+    [4, 5, 6]
+  ]);
+  let result2 = transpose([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+  ]);
+
+  return {
+    result1,
+    result2
+  };
+});
